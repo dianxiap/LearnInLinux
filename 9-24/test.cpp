582707496b4c3e9e8a1093b74857fb053e2b44f8
your-highness-p <1207864396@qq.com>
2023-09-24 20:41:44 +0800

    配置vscode环境

---
+++ 9-24/test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+int main()
+{
+    int a=10;
+    int b=20;
+    std::cout<<"hello vscode"<<endl;
+    std::cout<<"hello vscode"<<endl;
+    std::cout<<"hello vscode"<<endl;
+    std::cout<<"hello vscode"<<endl;
+    std::cout<<"hello vscode"<<endl;
+    std::cout<<"hello vscode"<<endl;
+    return 0;
+}
