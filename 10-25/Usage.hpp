48223b4a447b766da8e1be97f559125f13b56b73
your-highness-p <1207864396@qq.com>
2023-10-25 22:58:22 +0800

    http协议

---
+++ 10-25/Usage.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+void Usage(std::string proc)
+{
+    std::cout << "\nUsage: " << proc <<  " port\n" << std::endl;
+}
---
+++ 10-25/Util.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+
+class Util
+{
+public:
+    // aaaa\r\nbbbbb\r\nccc\r\n\r\n
+    static void cutString(std::string s, const std::string &sep, std::vector<std::string> *out)
+    {
+        std::size_t start = 0;
+        while (start < s.size())
+        {
+            auto pos = s.find(sep, start);
+            if (pos == std::string::npos)
+                break;
+            std::string sub = s.substr(start, pos - start);
+            // std::cout << "----" << sub << std::endl;
+            out->push_back(sub);
+            start += sub.size();
+            start += sep.size();
+        }
+        if (start < s.size())
+            out->push_back(s.substr(start));
+    }
+};
