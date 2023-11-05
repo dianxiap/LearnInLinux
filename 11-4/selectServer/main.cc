30066432c6589653b6ab0d1b083588d661bc98ee
your-highness-p <1207864396@qq.com>
2023-11-05 20:07:31 +0800

    selectServer

--- 11-4/selectServer/main.cc
+++ 11-4/selectServer/main.cc
@@ -1,20 +1,13 @@
 #include "selectServer.hpp"
-using namespace std;
+#include <memory>
 
 int main()
 {
-    //1.fd_set是一个固定大小的位图，直接决定了select能同时关心的fd的个数是有限的
-    cout<<sizeof(fd_set)*8<<endl;
-    // while (1)
-    // {
-    //     cout << "time: " << (unsigned long)time(nullptr) << endl;
-    //     struct timeval currtime = {0, 0};
-    //     int n = gettimeofday(&currtime, nullptr);
-    //     assert(n == 0);
-    //     (void)n;
+    // 1. fd_set是一个固定大小位图，直接决定了select能同时关心的fd的个数是有上限的！
+    // std::cout << sizeof(fd_set) * 8 << std::endl;
 
-    //     cout << "gettimeofday: " << currtime.tv_sec << "." << currtime.tv_usec << endl;
-    //     sleep(1);
-    // }
+    std::unique_ptr<SelectServer> svr(new SelectServer());
+    svr->Start();
+    
     return 0;
 }
